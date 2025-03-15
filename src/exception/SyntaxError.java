40a35b177e2cbe1e9d79da37ce967428b2355c5c
Supunsan <supun-san@users.noreply.github.com>
2025-03-15 17:19:48 +0530

    Update SyntaxError.java

--- src/exception/SyntaxError.java
+++ src/exception/SyntaxError.java
@@ -2,7 +2,8 @@ package exception;
 
 public class SyntaxError {
     public static void main(String[] args) {
-        System.out.println("SyntaxError.main");
+//        System.out.println("SyntaxError.main") //This line syntax error
+        System.out.println("SyntaxError.main"); //Fixed the error
     }
 }
 
